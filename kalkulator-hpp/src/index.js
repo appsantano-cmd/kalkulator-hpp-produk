36814755520dc624d38dbde997c85f9f01b4de7a
Adhiel Asiabel <vincent.adhiel28@gmail.com>
2026-01-14 12:00:23 +0700

    refactor: rumus dan UI

--- kalkulator-hpp/src/index.js
+++ kalkulator-hpp/src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Tambahkan Bootstrap Icons
+const link = document.createElement('link');
+link.href = 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css';
+link.rel = 'stylesheet';
+document.head.appendChild(link);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
